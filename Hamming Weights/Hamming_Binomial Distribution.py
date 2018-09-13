4ef2cd0cbbeb2b935fc7a571ea90b5ce590edf11
Hamza Qureshi <40399135+thehamzaq@users.noreply.github.com>
2018-09-12 18:20:19 -0600

    Update Hamming_Binomial Distribution.py

--- Hamming Weights/Hamming_Binomial Distribution.py
+++ Hamming Weights/Hamming_Binomial Distribution.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[2]:
+# In[1]:
 
 
 from sympy import Matrix, I
@@ -16,22 +16,26 @@ from scipy import stats
 #import itertools
 #from itertools import combinations
 
-#N = input("N = ")
-N=15
+
+# In[49]:
+
+
+#Assumptionless Binomial
+N = input("N = ")
 state_before = range(N+1)
 for i in xrange(0,N+1):
     state_before[i] = scipy.special.binom(N, i)/2**N
 
-for h in xrange(0,20):
-    r=h*0.05
+for h in xrange(0,21):
+    r=h*0.025
     t=1.-r
 
-    Strings = range(N+1)
+    #Strings = range(N+1)
 #Output_Prob = np.zeros((N+1,N+1))
     Hamming = np.zeros(N+1)
 
-    for i in xrange(0,N+1):
-        Strings[i]= [int(j) for j in list('0'*(N-i)+'1'*i)]
+    #for i in xrange(0,N+1):
+        #Strings[i]= [int(j) for j in list('0'*(N-i)+'1'*i)]
 
 #print(Strings)
 
@@ -55,11 +59,13 @@ for h in xrange(0,20):
     def compare_listcomp(x, y):
         return [i for i, j in zip(x, y) if i == j]
 
-    for i in xrange(0,N+1):
+    for i in xrange(0,2**N):
         for j in xrange(0,2**N):
-            Trans = len(compare_listcomp(Strings[i], comb[j]))
+            Trans = len(compare_listcomp(comb[i], comb[j]))
             Hamming[int(sum(comb[j]))] = (Hamming[int(sum(comb[j]))] + 
-            (t**Trans)*(r**(N-Trans))*scipy.special.binom(N, i)/2**N)
+            (t**Trans)*(r**(N-Trans)))
+            
+    Hamming = np.divide(Hamming, np.sum(Hamming))
 
 #print(Hamming)
     plt.plot(list(range(0,N+1)), state_before, 'ro', label='Binomial')
@@ -69,6 +75,3 @@ for h in xrange(0,20):
     plt.ylabel('Prob')
     plt.title('N = %s and r = %s' %(N,r))
     plt.show()
-    
-    print ("Cumulants (Hamming): %s, %s, %s, %s" % (stats.kstat(Hamming, 1), stats.kstat(Hamming, 2), 
-       stats.kstat(Hamming, 3), stats.kstat(Hamming, 4)))
